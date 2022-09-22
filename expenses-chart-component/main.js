4ea65aa98f452c110d39eec6551eb763cb702c25
Adam_M <adm.mzkr@gmail.com>
2022-09-22 13:54:30 +0200

    add js file to draw chart

---
+++ expenses-chart-component/main.js
@@ -0,0 +1,60 @@
+const dataJONSON = [
+  {
+    day: "mon",
+    amount: 17.45,
+  },
+  {
+    day: "tue",
+    amount: 34.91,
+  },
+  {
+    day: "wed",
+    amount: 52.36,
+  },
+  {
+    day: "thu",
+    amount: 31.07,
+  },
+  {
+    day: "fri",
+    amount: 23.39,
+  },
+  {
+    day: "sat",
+    amount: 43.28,
+  },
+  {
+    day: "sun",
+    amount: 25.48,
+  },
+];
+const daysArr = [];
+const amountArr = [];
+
+dataJONSON.forEach((el) => {
+  daysArr.push(el.day);
+  amountArr.push(el.amount);
+});
+const data = {
+  labels: daysArr,
+  datasets: [
+    {
+      data: amountArr,
+      backgroundColor: "hsl(186, 34%, 60%)",
+    },
+  ],
+};
+
+const config = {
+  type: "bar",
+  data: data,
+  options: {
+    plugins: {
+      legend: {
+        display: false,
+      },
+    },
+  },
+};
+// };
+const myChart = new Chart(document.getElementById("myChart"), config);
